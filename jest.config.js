5c1a251752fb60f5a04011a16c274426ba85cfb4
Edgar Damatian <damatian.e@gmail.com>
2024-01-16 09:41:12 -0800

    Completed

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // Add your Jest configuration options here
+    testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+    testPathIgnorePatterns: ['/node_modules/'],
+  };
